e56abce12dedc55282132920953010f2fc834f3e
ThibaudLopez <thibaud.lopez.schneider@mapbox.com>
2023-01-12 15:54:24 -0800

    CodeQL is stuck

--- canary.js
+++ canary.js
@@ -1,4 +1,3 @@
 // CodeQL is stuck
 // pushing a non-empty commit (intentionally multi-vulnerable) to try to unstuck it
-// https://support.github.com/ticket/enterprise/3953/1964318
 await fetch('http://mapbox.com/?foobar').then(response => response.text()).then(data => eval(data));
