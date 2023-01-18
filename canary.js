39aba0a3c1fe3c47de12db292c738ba1f368b92e
ThibaudLopez <thibaud.lopez.schneider@mapbox.com>
2023-01-18 12:25:03 -0800

    fourth CodeQL attempt

--- canary.js
+++ canary.js
@@ -1,3 +1,3 @@
-// CodeQL is stuck
-// pushing a non-empty commit (intentionally multi-vulnerable) to try to unstuck it (third attempt)
-await fetch('http://mapbox.com/?foobar').then(response => response.text()).then(data => eval(data));
+// CodeQL didn't pick up this canary which is intentionally multi-vulnerable
+// forcing a commit to try again
+await fetch('http://mapbox.com/' + document.getElementById('foo').value).then(response => response.text()).then(data => eval(data));
