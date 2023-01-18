8c416d63ef7386765ff28206d5ba2c05b8aa2110
ThibaudLopez <thibaud.lopez.schneider@mapbox.com>
2023-01-18 12:32:27 -0800

    CodeQL still didn't pick-up the canary...deleting the canary

--- canary.js
+++
@@ -1,3 +0,0 @@
-// CodeQL didn't pick up this canary which is intentionally multi-vulnerable
-// forcing a commit to try again
-await fetch('http://mapbox.com/' + document.getElementById('foo').value).then(response => response.text()).then(data => eval(data));
